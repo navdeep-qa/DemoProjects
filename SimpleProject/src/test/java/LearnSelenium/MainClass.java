cffba9fab0a22fce068f859d2832d24cd6ee369c
Navdeep.Kaur <Navdeep.Kaur@TNINCHD030.oca.internal>
2019-08-08 11:29:12 +0530

    First time push to the git

---
+++ SimpleProject/src/test/java/LearnSelenium/MainClass.java
@@ -0,0 +1,30 @@
+package LearnSelenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MainClass {
+
+	public static WebDriver driver;
+	
+	
+	
+	@Test
+	void LaunchBrowser()
+	{
+//		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+//		driver = new ChromeDriver();
+//		driver.get("http://oca-mel-test:8300/");
+		
+		System.out.println("test1");
+		
+	}
+	
+	@Test
+	void Test2()
+	{
+		System.out.println("test2");
+	}
+	
+}
